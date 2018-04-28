b8bba63bb18431c25feb0cc0e4840111a790010f
jianglinshan <jianglinshan@qiyi.com>
2018-04-28 18:09:49 +0800

    算法改进

--- LongestPalindromic/main.cpp
+++ LongestPalindromic/main.cpp
@@ -1,32 +1,33 @@
 #include <iostream>
 using namespace std;
 
-bool isPalindromic(string s){
-    int length = s.length();
-    for (int i = 0;i < length/2;i++)
+bool isPalindromic(string s,int start, int end){
+    for (int i = start;i < (end + start + 1)/2;i++)
     {
-        if (s[i] != s[length - i - 1])
+        if (s[i] != s[end + start - i])
             return false;
     }
     return true;
 }
 
-string longestPalindromicSubString(string s)
-{
+string longestPalindrome(string s) {
     string ret = string("");
-    for (int i = 0; i < s.length(); ++i) {
-        for (int j = i+1; j < s.length() ; ++j) {
-            string sub = s.substr(i,j - i + 1);
-            if (isPalindromic(sub) && sub.length() > ret.length())
-                ret = sub;
+    int length = s.length(),retLength = 0;
+    for (int i = 0; i < length - retLength; ++i) {
+        for (int j = i+retLength ; j < length ; ++j) {
+            if (isPalindromic(s,i,j))
+            {
+                ret = s.substr(i,j - i + 1);
+                retLength = j-i+1;
+            }
         }
     }
     return ret;
 }
 
 int main() {
-    string s = string("saljfdlasjglasjglwojsldnvosaofhlfslajflsfowewdfasdg");
-    string sub = longestPalindromicSubString(s);
+    string s = string("sdgsadgasdgeddddsadgwegdg");
+    string sub = longestPalindrome(s);
     printf("%s",&sub);
     return 0;
 }
